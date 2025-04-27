b265b71f1f0a93acea6d34220a8b59b6a43b40ac
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2025-04-27 16:42:12 +0200

    Exercici 2 i 3

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Diagnostics;
+
+namespace T2Activitats
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //Ex 2
+            string fileRouteEx2 = "../../../processList.txt";
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "dotnet",
+                    Arguments = "--info",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            Console.WriteLine(output);
+
+            var processos = Process.GetProcesses();
+            using (StreamWriter escriptor = new StreamWriter(fileRouteEx2))
+            {
+                foreach (Process os in processos)
+                {
+                    Console.WriteLine($"PID: {os.Id}, Name: {os.ProcessName}");
+                    escriptor.WriteLine($"PID: {os.Id}, Name: {os.ProcessName}");
+                }
+            }
+
+            //Ex 3
+            string fileRouteEx3 = "../../../one_process.txt";
+            Console.WriteLine("Choose a process: (PID)");
+            int pid = int.Parse(Console.ReadLine());
+            Process chromeP = null;
+            try
+            {
+                chromeP = Process.GetProcessById(pid);
+                ProcessThreadCollection pTC = chromeP.Threads;
+                Console.WriteLine("Threats of the program {0}, Threats count: {1}", chromeP.ProcessName, pTC.Count);
+                using (StreamWriter escriptor = new StreamWriter(fileRouteEx3))
+                {
+                    foreach (ProcessThread pt in pTC)
+                    {
+                        Console.WriteLine($"{pt.Id} \t Container {pt.Container}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            try
+            {
+                ProcessModuleCollection pMC = chromeP.Modules;
+                Console.WriteLine("Modul of the program {0}, ModulCount: {1}", chromeP.ProcessName, pMC.Count);
+                using (StreamWriter escriptor = new StreamWriter(fileRouteEx3))
+                {
+                    foreach (ProcessModule pM in pMC)
+                    {
+                        Console.WriteLine($"{pM.ModuleName} \t Container: {pM.Container}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+
+}
