fdeb7091e5afa72092b0e3268fc50bcef00f5693
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2025-04-27 16:51:20 +0200

    Exercices in C#

---
+++ Ex5.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2Activitats
+{
+    public class Ex5
+    {
+        public static void MainEx5()
+        {
+            //Ex5a
+            Thread[] threadsEx5a = new Thread[5];
+            for (int i = 0; i < 5; i++)
+            {
+                int num = i + 1;
+                threadsEx5a[i] = new Thread(() =>
+                {
+                    Console.WriteLine($"Hola! Sóc el fil número {num}");
+                });
+            }
+            foreach (var t in threadsEx5a) { t.Start(); }
+
+            foreach (var t in threadsEx5a) { t.Join(); }
+
+            //Ex5b
+            Thread[] threadsEx5b = new Thread[5];
+            for (int i = 0; i < 5; i++)
+            {
+                int num = i + 1;
+                threadsEx5b[i] = new Thread(() =>
+                {
+                    Thread.Sleep((6 - num) * 100);
+                    Console.WriteLine($"Hola! Soc el fil número {num}");
+                });
+            }
+
+            foreach (var t in threadsEx5b) { t.Start(); }
+
+            foreach (var t in threadsEx5b) { t.Join(); }
+        }
+
+    }
+}
---
+++ Ex6.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2Activitats
+{
+    public class Camel
+    {
+        private int Id;
+        private int MinSleep, MaxSleep;
+
+        public Camel(int id, int minSleep, int maxSleep)
+        {
+            Id = id;
+            MinSleep = minSleep;
+            MaxSleep = maxSleep;
+        }
+
+        public void Run()
+        {
+            var rand = new Random(Guid.NewGuid().GetHashCode());
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine($"Camell {Id}: {i}");
+                int ms = rand.Next(MinSleep, MaxSleep + 1);
+                Thread.Sleep(ms);
+            }
+        }
+    }
+    public class Ex6
+    {
+        public static void Main()
+        {
+            var camells = new Thread[5];
+            for (int i = 0; i < 5; i++)
+            {
+
+                int min = 10 + i * 5;
+                int max = min + 20;
+                var c = new Camel(i + 1, min, max);
+                camells[i] = new Thread(c.Run);
+            }
+
+            foreach (var t in camells) t.Start();
+            foreach (var t in camells) t.Join();
+
+            Console.WriteLine("Carrera acabada!");
+        }
+    }
+}
